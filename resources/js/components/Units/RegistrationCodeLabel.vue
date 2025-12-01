484d108bafda17c0e27a535a12d21e88dea2a82a
Tim Kolijn <tim@kolijn.org>
2025-12-02 00:28:40 +0100

    Fix label layout

--- resources/js/components/Units/RegistrationCodeLabel.vue
+++ resources/js/components/Units/RegistrationCodeLabel.vue
@@ -22,7 +22,7 @@ if (unit_split.length < 3) {
 <template>
     <div
         id="label"
-        class="flex h-[100px] w-[340px] items-center justify-between overflow-hidden border border-gray-300 bg-white p-2 text-black shadow-lg"
+        class="flex h-[110px] w-[340px] items-center justify-between overflow-hidden border border-gray-300 bg-white p-2 text-black shadow-lg"
     >
         <div class="flex h-full w-full items-center justify-between px-0">
             <div
--- resources/js/pages/dashboard/registrationCodes/Index.vue
+++ resources/js/pages/dashboard/registrationCodes/Index.vue
@@ -20,7 +20,7 @@ const flatLabelData = computed(() => {
 <template>
     <div
         id="label"
-        class="flex h-[100px] w-[340px] items-center justify-between overflow-hidden border border-gray-300 bg-white p-2 text-black shadow-lg"
+        class="flex h-[110px] w-[340px] items-center justify-between overflow-hidden border border-gray-300 bg-white p-2 text-black shadow-lg"
     >
         <div class="flex h-full w-full items-center justify-between px-0">
             <div
