8a97f6c6f1e8a31fbd90e7e1e08bb595982a8587
Tim Kolijn <tim@kolijn.org>
2025-12-05 02:11:46 +0100

    Add Rooms route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
 use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -8,6 +9,8 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home', []);
 })->name('home');
+
+Route::resource('rooms', RoomController::class);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('profile', [ProfileController::class, 'index'])->middleware(['auth'])->name('profile');
