a3cde3b288c48d9b95cc7d7d73e3e110096f5b7b
Tim Kolijn <tim@kolijn.org>
2026-03-06 15:46:18 +0100

    Fix Pint

--- app/Http/Controllers/Public/AgendaController.php
+++ app/Http/Controllers/Public/AgendaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
 use App\Models\Agenda;
-use App\Models\AgendaItem;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,16 +12,17 @@ class AgendaController extends Controller
     public function index(Request $request)
     {
         $agendas = Agenda::query()
-            ->when(!$request->user(), function ($query) {
+            ->when(! $request->user(), function ($query) {
                 $query->where('public', true);
             })
             ->get();
+
         return Inertia::render('agenda/Index', compact('agendas'), []);
     }
 
     public function show(Request $request, Agenda $agenda)
     {
-        if (!($request->user() || $agenda->public)) {
+        if (! ($request->user() || $agenda->public)) {
             abort(404);
         }
         $agendaItems = $agenda->agendaItems()
