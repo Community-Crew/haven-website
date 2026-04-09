58122d37bd88bcd57f8619a62a3babe7c9428012
Tim Kolijn <tim@kolijn.org>
2026-04-09 20:25:33 +0200

    Fix room not routing correctly

--- app/Http/Controllers/Public/ReservationController.php
+++ app/Http/Controllers/Public/ReservationController.php
@@ -36,7 +36,7 @@ class ReservationController extends Controller
         $reservation = new Reservation;
         $this->saveReservation($reservation, $validated, $request->user()->id);
 
-        return redirect()->route('rooms.show', $room->id)->with('success', 'Created!');
+        return redirect()->route('rooms.show', $room->slug)->with('success', 'Created!');
     }
 
     private function saveReservation(Reservation $reservation, array $data, int $userId): void
@@ -48,7 +48,7 @@ class ReservationController extends Controller
             'start_at' => $this->parseDateTime($data['start_time']),
             'end_at' => $this->parseDateTime($data['end_time']),
             'share_user' => $data['share_name'],
-            'organisation_id' => $data['organisation'], // Unified key
+            'organisation_id' => $data['organisation'],
             'status' => $reservation->exists ? $reservation->status : ReservationStatus::APPROVED,
         ]);
         $reservation->save();
