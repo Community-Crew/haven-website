1b0e81f94d5c10cca635def26e41767f83116c47
Tim Kolijn <tim@kolijn.org>
2026-07-12 01:57:41 +0200

    Big ass cleanup

--- app/Services/ReservationPolicyService.php
+++ app/Services/ReservationPolicyService.php
@@ -5,7 +5,6 @@ namespace App\Services;
 use App\Models\ReservationPolicy;
 use App\Models\ReservationPolicyEntry;
 use App\Models\Room;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
